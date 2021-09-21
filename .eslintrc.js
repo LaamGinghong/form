e6b49fafd75776afbed862b24f23ad00ed8a0754
LaamGinghong <351390560@qq.com>
2021-09-21 10:58:39 +0800

    feat: initial

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+const path = require('path')
+
+module.exports = {
+  env: {
+    browser: true,
+    es2020: true,
+    node: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard',
+    'plugin:import/typescript',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  settings: {
+    'import/resolver': {
+      typescript: path.resolve(__dirname, './tsconfig.json'),
+    },
+  },
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  rules: {},
+}
